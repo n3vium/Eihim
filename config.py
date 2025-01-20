0cd1acba239092c0fa42f282f8500dbc589ef471
d3velo <biryuknikita842@gmail.com>
2025-01-20 20:34:48 +0200

    Забыл выгрузить конфиг

---
+++ config.py
@@ -0,0 +1,18 @@
+# Учетные данные Spotify
+SPOTIFY_CLIENT_ID = '99b7bc5775984103965287a4398a5de5'
+SPOTIFY_CLIENT_SECRET = '7c967505fab04bf2921b1386be69dc24'
+SOUNDCLOUD_CLIENT_ID = 'meQQRL0IxbE5bGwr7b9pBtluA7WtDzYX'
+BOT_TOKEN = '7607772085:AAGPipGBxXH5m7-dmzHET_KLBANHMNdsjsI'
+
+# Настройки загрузки
+DOWNLOAD_DIR = './downloads'
+AUDIO_FORMAT = 'mp3'
+AUDIO_QUALITY = '192'
+
+# Поддерживаемые платформы
+SUPPORTED_PLATFORMS = {
+    'spotify': ['open.spotify.com', 'spotify:track:'],
+    'youtube': ['youtube.com', 'youtu.be'],
+    'soundcloud': ['soundcloud.com'],
+    'deezer': ['deezer.com']
+} 
